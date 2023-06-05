36a8728da0834686af46b329727701a363eda57f
Nicholas.Hubacek <Nicholas.Hubacek@gmail.com>
2023-06-05 10:35:12 -0500

    completed HomeController

--- src/main/java/com/codeup/codeupspringblog/controllers/HomeController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/HomeController.java
@@ -1,4 +1,13 @@
 package com.codeup.codeupspringblog.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+@Controller
 public class HomeController {
+    @GetMapping("/home")
+    @ResponseBody
+    public String home(){
+    return "<h1>This is the landing page!</h1>";
+    }
 }
