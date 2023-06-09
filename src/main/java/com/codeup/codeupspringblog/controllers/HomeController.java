1bd1a1a1479d149789495da540628905c17f1181
Nicholas.Hubacek <Nicholas.Hubacek@gmail.com>
2023-06-09 13:55:29 -0500

    forgot to push

--- src/main/java/com/codeup/codeupspringblog/controllers/HomeController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/HomeController.java
@@ -3,16 +3,13 @@ package com.codeup.codeupspringblog.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 @Controller
 public class HomeController {
-//    @GetMapping("/")
-//    @ResponseBody
-//    public String home(){
-//    return "<h1>This is the landing page!</h1>";
-//    }
-//}
-@GetMapping("/home")
-public String home(){
-return "home";
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
     }
+
 }
--- src/main/java/com/codeup/codeupspringblog/models/Post.java
+++ src/main/java/com/codeup/codeupspringblog/models/Post.java
@@ -2,6 +2,7 @@ package com.codeup.codeupspringblog.models;
 
 
 import jakarta.persistence.*;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,7 +14,7 @@ import lombok.Setter;
 @Setter
 
 @Entity
-//@Table(name="posts")
+@Table(name="posts")
 public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,7 +24,16 @@ public class Post {
     @Column(nullable = false, columnDefinition = "TEXT")
     private String body;
 
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
     public Post(String title, String body) {
+        this.title = title;
+        this.body = body;
+    }
+    public Post(User user, String title, String body) {
+        this.user = user;
         this.title = title;
         this.body = body;
     }
